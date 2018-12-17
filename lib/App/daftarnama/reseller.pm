baede0f0238e72fc4cd9a090b4b2c33a81a68155
perlancar (on pc-office) <perlancar@gmail.com>
2018-12-17 20:15:44 +0700

    Initial commit

---
+++ lib/App/daftarnama/reseller.pm
@@ -0,0 +1,16 @@
+package App::daftarnama::reseller;
+
+# DATE
+# VERSION
+
+1;
+# ABSTRACT: CLI to access reseller API on DaftarNama.id
+
+=head1 SYNOPSIS
+
+Please see included script L<daftarnama-reseller>.
+
+
+=head1 SEE ALSO
+
+L<WWW::DaftarNama::Reseller>
